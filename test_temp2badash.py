e5c8fd339c6b6721e0ed98195749ac0c79f68749
swilcox <steven@wilcoxzone.com>
2018-05-07 07:52:01 -0500

    add additional test for main()

--- test_temp2badash.py
+++ test_temp2badash.py
@@ -26,3 +26,18 @@ def test_send_temp():
         temp2badash._send_to_badash('test-job-slug', None)
         call_info = m.request_history[-1]
         assert call_info.json() == {'job': 'test-job-slug', 'result': -1, 'temperature': 0.0}
+
+
+def test_main():
+    temp2badash.BADASH_API_KEY = 'xxxx42xxxx'
+    temp2badash.BADASH_API_URL = 'http://badash-api.example.com/events'
+    temp2badash.JOB_NAME = 'test-job-slug'
+    temp2badash.FILENAME = './test_fixtures/good_example.data'
+    with requests_mock.mock() as m:
+        m.post('http://badash-api.example.com/events', status_code=201)
+        temp2badash.main()
+        assert m.called is True
+        assert m.call_count == 1
+        call_info = m.request_history[0]
+        assert call_info.json() == {'job': 'test-job-slug', 'result': 0, 'temperature': 68.9}
+
